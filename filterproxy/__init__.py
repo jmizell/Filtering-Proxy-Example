bea357d34e47d8032bf6aff8c1f4b4703defe3d5
Jeremy Mizell <mizellj@ottercove.net>
2017-04-28 08:59:18 -0600

    Adding comments on process

--- filterproxy/__init__.py
+++ filterproxy/__init__.py
@@ -8,6 +8,8 @@ from functools import wraps
 from datetime import datetime
 from sklearn.externals import joblib
 
+# Flask was chosen as it's ease of use in prototyping. Ideally, if python was still 
+# the language of choice, a library like tornado, or uvloop would be used instead.
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler())
 app.logger.setLevel(logging.INFO)
@@ -33,6 +35,11 @@ def log_request(view):
         request_path = request.full_path
         status = resp.status
         log_string = "%s - [%s] %s: \"%s\" - \"%s\", %s" % (ip, timestamp, method, request_path, user_agent, status)
+        
+        # I love logging everything if I can. For simplicity I'm just dumping everything to stdout in the container, 
+        # and I'm dumping the minimum of information. Ideally I'd dump this into some sort of message queue (sqs maybe?), 
+        # as a json object, containing a bit more information. Things like useragent, headers, etc. That way, if 
+        # there is an issue, we have a nice chunk of data in the audit trail to look at.
         app.logger.info(log_string)
         return resp
     return decorated
@@ -52,11 +59,22 @@ def dumb_sql_filter(view):
             # vectorize the request arguments values
             vectors = vectorizer.transform(request.args.values())
 
-            # check the predictions
+            # Here, I'm using an svm classifier, but that is not likely to be the ideal choice
+            # for a production environment. SVM doesn't scale to large data sets well. Most likely
+            # I'd imagine something like a random forest classifier would work best; something that
+            # could still be done efficiently on cpu.
             for prediction in classifier.predict(vectors):
                 status = app.config['CLASSES'][prediction]['status']
                 message = app.config['CLASSES'][prediction]['message']
 
+                # So, here if I really wanted to this well, classification would not be
+                # a boolean catagory. I'd get the probability from the classifiery, and then
+                # add that to a total score for all query parameters. You could then choose
+                # how strict you want your filter to be, by setting a threshold on the score.
+                
+                # I did not do that here, because that adds complexity to what is supposed to 
+                # a simple example. 
+                
                 # don't do anything with negatives
                 if status == 200:
                     continue
@@ -65,6 +83,7 @@ def dumb_sql_filter(view):
     return decorated
 
 
+# Only GET, no post, put, or anything else. 
 @app.route('/', methods=['GET'])
 @app.route('/<path:path>', methods=['GET'])
 @log_request
@@ -76,10 +95,20 @@ def root(path="/"):
     :param path: client request path
     :return: Response object containing the response from the upstream server
     """
+    
+    # This here, is probably the most inefficent way to handle this task. I'm storing everything
+    # in memory, then passing it on to the client. Ideally I'd stream the response to the client
+    # as it arrived from the upstream server. However, all attempts I've made to do that in 
+    # Flask in the past have had issues. Flask is just not suited for that task with out jumping
+    # through some hoops. So in a production situation, you'd want to use something else, like
+    # tornado for example.
     req = requests.get("%s/%s" % (app.config['PROXY_TARGET'], path), params=request.args, headers=request.headers)
     response_content = req.content
     content_type = req.headers.get('content-type', 'text/html')
     status = req.status_code
+    
+    # There's probably some headers I shouldn't be ignoring from the upstream server, but for now 
+    # just setting the content_type works.
     return Response(response_content, content_type=content_type, status=status)
 
 
